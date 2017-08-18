3f7b2d12ba6fe8e582eaf4f04044d53f3dcd600f
Wilson Santos <NB18726@novabase.pt>
2017-08-18 19:06:02 +0100

    Added the Databinding exmples

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
   styleUrls: ['./server.component.css']
 })
-export class ServerComponent implements OnInit {
+export class ServerComponent {
+  serverId: number = 10;
+  serverName: String ='Server';
+  serverStatus: String = 'Offline'
+  serverIp: String = '127.0.0.1'
 
-  constructor() { }
 
-  ngOnInit() {
+  getServerStatus(){
+    return this.serverStatus+"Method";
   }
 
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  serverName='Test Server Name';
+  allowNewServer = false;
+  serverCreation = 'No Server had Been Created';
 
-  constructor() { }
+  constructor() { 
+    setTimeout(() =>{
+      this.allowNewServer=true;
+    },2000);
+  }
 
   ngOnInit() {
+  }
+
+  onCreateServer(){
+    this.serverCreation = 'Server Was Created! Name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event: Event){
+      this.serverName=(<HTMLInputElement>event.target).value;
   }
 
 }
